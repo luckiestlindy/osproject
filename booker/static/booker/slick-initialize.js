8f73af841d8523565e7942d552b06d05ef850a4e
luckieslindy <brentlind@gmail.com>
2017-02-26 18:21:26 -0600

    added carousel

---
+++ booker/static/booker/slick-initialize.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    $('.carousel').slick({
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        speed: 500,
+        fade: true,
+        cssEase: 'linear',
+        pauseOnFocus: false
+    });
+});
