84f14b9cde5d195464306a5c11111c5d71f7091c
Brent Lind <brentlind@gmail.com>
2017-03-09 11:57:57 -0600

    removed hypen from directory name

--- oreadstrings/wsgi.py
+++ oreadstrings/wsgi.py
@@ -11,9 +11,8 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oreadstrings.settings")
 
 application = get_wsgi_application()
-
-# from whitenoise.django import DjangoWhiteNoise
 application = DjangoWhiteNoise(application)
