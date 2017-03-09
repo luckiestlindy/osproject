0f2e1f89e2d55103702d2718bab308552577be81
Brent Lind <brentlind@gmail.com>
2017-03-09 10:13:12 -0600

    fixed gunicorn file

--- oreadstrings/wsgi.py
+++ oreadstrings/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oreadstrings.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oread-strings.settings")
 
 application = get_wsgi_application()
 
