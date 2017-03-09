0c219533cb7d37b11667a48b250ba348d81618fe
Brent Lind <brentlind@gmail.com>
2017-03-09 10:39:07 -0600

    changed back to oreadstrings.settings

--- oreadstrings/wsgi.py
+++ oreadstrings/wsgi.py
@@ -11,7 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oreadstrings.settings")
 
 application = get_wsgi_application()
 
