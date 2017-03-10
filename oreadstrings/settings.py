46f96322639353896a3fa1e31c5aff9319bbcfe6
Brent Lind <brentlind@mac.com>
2017-03-10 08:45:08 -0600

    a

--- oreadstrings/settings.py
+++ oreadstrings/settings.py
@@ -21,32 +21,12 @@ import dj_database_url
 
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 BASE_DIR = os.path.dirname(PROJECT_DIR)
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.9/howto/static-files/
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
-
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
-
-STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
-
-
-
-
-
-THUMBNAIL_PROCESSORS = (
-    'image_cropping.thumbnail_processors.crop_corners',
-) + thumbnail_settings.THUMBNAIL_PROCESSORS
-
+DEBUG = False
 
 
 # Application definition
@@ -98,6 +78,8 @@ TEMPLATES = [
 WSGI_APPLICATION = 'oreadstrings.wsgi.application'
 
 
+DATABASES = { 'default': dj_database_url.config() }
+
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
@@ -121,26 +103,27 @@ AUTH_PASSWORD_VALIDATORS = [
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'America/Chicago'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
+TIME_INPUT_FORMATS = ('%I:%M %p',) #will format your times as "5:30 PM"
 
 
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+ALLOWED_HOSTS = ['*']
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
-STATIC_URL = '/static/'
+
 STATIC_ROOT = os.path.join(BASE_DIR, "booker/static")
+STATIC_URL = '/static/'
 
-MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "booker/media")
+MEDIA_URL = '/media/'
 
-TIME_INPUT_FORMATS = ('%I:%M %p',) #will format your times as "5:30 PM"
 
 # EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # EMAIL_HOST = os.environ['EMAIL_HOST']
@@ -223,14 +206,28 @@ LOGGING = {
     },
 }
 
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'
+
+
+
+
+
+THUMBNAIL_PROCESSORS = (
+    'image_cropping.thumbnail_processors.crop_corners',
+) + thumbnail_settings.THUMBNAIL_PROCESSORS
 
 # DATABASES['default'] = dj_database_url.config()
-DATABASES = { 'default': dj_database_url.config() }
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-ALLOWED_HOSTS = ['*']
 
-DEBUG = False
+
 
 try:
     from .local_settings import *
