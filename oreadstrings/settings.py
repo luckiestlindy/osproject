7316b168e6567d38d5fedff5a4a4404db89c0607
Brent Lind <brentlind@mac.com>
2017-03-14 09:38:58 -0500

    more changes to aws env vars

--- oreadstrings/settings.py
+++ oreadstrings/settings.py
@@ -131,6 +131,11 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_S3_SECURE_URLS = False       # use http instead of https
 AWS_QUERYSTRING_AUTH = False   
 AWS_S3_HOST = "s3-us-east-2.amazonaws.com"
+AWS_S3_ACCESS_KEY_ID = os.environ['AWS_S3_ACCESS_KEY_ID']
+AWS_S3_SECRET_ACCESS_KEY = os.environ['AWS_S3_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
+AWS_S3_CUSTOM_DOMAIN = os.environ['AWS_S3_CUSTOM_DOMAIN']
+
 
 MESSAGE_TAGS = {
     message_constants.DEBUG: 'debug',
