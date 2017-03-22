45393b203447cb7b0d509c2e343048102a9c6809
Brent Lind <brentlind@mac.com>
2017-03-22 16:04:19 -0500

    updating settings

--- oreadstrings/settings.py
+++ oreadstrings/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'weasyprint',
     'storages',
     'versatileimagefield',
+    'paypal.standard.ipn',
     
 
 ]
@@ -98,7 +99,8 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
+PAYPAL_TEST = True
+PAYPAL_RECIEVER_EMAIL = 'oreadstrings-facilitator@gmail.com'
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.10/topics/i18n/
