d3e04ffac29f6586f1384b6611f389dd6ae78624
Brent Lind <brentlind@mac.com>
2017-03-23 09:25:41 -0500

    a

--- booker/views.py
+++ booker/views.py
@@ -20,7 +20,7 @@ from paypal.standard.ipn.signals import valid_ipn_received
 import logging
 
 # Get an instance of a logger
-logger = logging.getLogger(sentry)
+logger = logging.getLogger(os.heroku)
 
 # Create your models here.
 def show_me_the_money(sender, **kwargs):
