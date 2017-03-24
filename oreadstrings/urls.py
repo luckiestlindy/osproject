8e82d90790f8ee9bca434d8ea87653e487bb2b4b
Brent Lind <brentlind@mac.com>
2017-03-24 08:36:35 -0500

    a

--- oreadstrings/urls.py
+++ oreadstrings/urls.py
@@ -28,3 +28,9 @@ urlpatterns = [
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += staticfiles_urlpatterns()
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns = [
+        url(r'^__debug__/', include(debug_toolbar.urls)),
+    ] + urlpatterns
