c93a4436a76c56188b1e585ce79c94e0c59130ea
Brent Lind <brentlind@mac.com>
2017-04-12 08:50:17 -0500

    major static and media updates

--- oreadstrings/settings.py
+++ oreadstrings/settings.py
@@ -121,7 +121,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')
 
 # AWS S3 Static Settings
-DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
 AWS_QUERYSTRING_AUTH = False
 AWS_S3_HOST = "s3-us-east-2.amazonaws.com"
 AWS_S3_ACCESS_KEY_ID = os.environ.get('AWS_S3_ACCESS_KEY_ID')
@@ -134,12 +134,22 @@ AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
     }
 
 # Static/Media Settings
-THUMBNAIL_DEFAULT_STORAGE = DEFAULT_FILE_STORAGE
 STATIC_ROOT = os.path.join(BASE_DIR, "booker/static")
 MEDIA_ROOT = os.path.join(BASE_DIR, "booker/media")
 MEDIA_URL = '/media/'
-STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
-STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+# STATIC_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+# STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+STATICFILES_LOCATION = 'static'
+STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, STATICFILES_LOCATION)
+
+MEDIAFILES_LOCATION = 'media'
+MEDIA_URL = 'https://%s/%s' % (AWS_S3_CUSTOM_DOMAIN, MEDIAFILES_LOCATION)
+DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+
+THUMBNAIL_DEFAULT_STORAGE = DEFAULT_FILE_STORAGE
+
 
 # Message Settings
 MESSAGE_TAGS = {
