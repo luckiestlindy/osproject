47d27d336cb4e0f24c5f75d6ce0039a509dc63c6
Brent Lind <brentlind@mac.com>
2017-04-24 17:01:22 -0500

    a

--- booker/views.py
+++ booker/views.py
@@ -321,7 +321,7 @@ def contract_link(request, pk):
 def notifyadmin(request, pk):
     event = get_object_or_404(Event, pk=pk)
     subject = 'New Booking Inquiry at Oreadstrings.com'
-    to = [os_admin_email]
+    to = [os_admin_email_to]
     message = 'You have a new booking inquiry from {0} for an event on {1}. Please click the link to see the details'.format(event.client_name, event.event_date)
     link = '{0}/admin/booker/event/{1}/change/'.format(base_url, event.pk)
     context = {
