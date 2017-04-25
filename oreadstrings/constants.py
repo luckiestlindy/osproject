a5c326c5972a817c33413c684c693882257986d9
Brent Lind <brentlind@mac.com>
2017-04-25 10:24:05 -0500

    a

--- oreadstrings/constants.py
+++ oreadstrings/constants.py
@@ -1,8 +1,8 @@
 os_admin_email = 'Oread Strings <postmaster@mg.oreadstrings.com>'
 os_admin_email_to = 'Oread Strings <brentlind@mac.com>'
 paypal_reciever_email = 'oreadstrings@gmail.com'
-paypal_logo_url = "https://s3.us-east-2.amazonaws.com/oreadstrings-assets/booker/images/site/fullcolor_pp.png"
-# paypal_logo_url = "{% url %}"
+# paypal_logo_url = "https://s3.us-east-2.amazonaws.com/oreadstrings-assets/booker/images/site/fullcolor_pp.png"
+paypal_logo_url = "{% static 'booker/images/site/fullcolor_pp.png' %}"
 base_url = 'https://oreadstrings.herokuapp.com'
 # base_url = 'http://127.0.0.1:8000'
 image_url_base = 'https://s3.us-east-2.amazonaws.com/oreadstrings-assets/booker'
