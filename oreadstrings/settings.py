b52aa699038f357d8a083a98610db9c6d1e31a58
Brent Lind <brentlind@gmail.com>
2021-11-01 13:15:21 -0500

    Update settings.py

--- oreadstrings/settings.py
+++ oreadstrings/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'htmlmin.middleware.HtmlMinifyMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
